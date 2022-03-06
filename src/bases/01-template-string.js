b26fc07c71e07c4a54a18587fd133b83fc247214
Danieltq04 <daniel.tq04@gmail.com>
2022-03-05 22:56:33 -0300

    bases para testear

---
+++ src/bases/01-template-string.js
@@ -0,0 +1,18 @@
+
+
+
+const nombre   = 'Pepe';
+const apellido = 'Ramos';
+
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${ nombre } ${ apellido }`;
+
+console.log( nombreCompleto );
+
+
+function getSaludo(nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
