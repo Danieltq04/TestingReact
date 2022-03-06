5d9b71ecba59881dcf2ee2f8b4bfd35a3510575b
Danieltq04 <daniel.tq04@gmail.com>
2022-03-06 18:15:46 -0300

    primera prueba con jest

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,11 +1,18 @@
 
 //npm i -D --exact jest-watch-typeahead@0.6.5
 
-test('Debe ser true', () => { 
-    const isActive = true;
+describe('pruebas en el archivo demo.test.js', () => { 
+    test('Debe ser iguales los strings', () => { 
+        //1. Inicialización
+        const mensaje = "Hola mundo";
+    
+        //2. Estímulo
+        const mensaje2 = `Hola mundo`
+        
+        //3. Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    
+    })
+    
+ })
 
-    if(isActive){
-        throw new Error("Error1")
-    }
-
-})
